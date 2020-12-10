3a75a2d453b0d942bec80d2b4f0e013e91f3d722
RobertoCarrichi <robertocarrichiunam@gmail.com>
2020-12-10 06:19:22 -0600

    "Update"

--- Batalla.java
+++ Batalla.java
@@ -6,11 +6,27 @@ public class Batalla{
 	*/
 	// Menu menus = new Menu();
 	public static void main(String[] args) {
-		System.out.println("INICIA EL COMBATE!");
 
-		
+		System.out.println("\n\t############### INICIANDO POKEJUEGO ############### \n");
+		try {
+			System.out.println("Estamos preparando todo...");
+			// Thread.sleep(3000);
+			System.out.println("LISTO! Es momento de comenzar!");
+			// Thread.sleep(1500);
+			System.out.println("Comenzaremos por la creacion de los dos jugadores que van a combatir...");
+			// Thread.sleep(1800);
 
-
+			/*************************************************************** 
+			* INICIA LA CREACION Y ASIGNACION DE POKEMON PARA CADA JUGADOR *
+			****************************************************************/
+			Jugador jugador1 = new Jugador(1);
+			// Thread.sleep(1650);
+			// Jugador jugador2 = new Jugador(2);
+			
+		}catch (Exception e) {
+			System.out.println("Al parecer tenemos problemas al cargar contenido del juego.");
+			e.printStackTrace();
+		}
 	}
 
 }
--- Jugador.java
+++ Jugador.java
@@ -1,10 +1,68 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Jugador {
+    // Instancia de apoyo para la lectura de datos.
+    Scanner scan = new Scanner(System.in);
+    
+    // ATRIBUTOS DE INSTANCIA
+    private String nombre;
     ArrayList<Pokemon> pokemones = new ArrayList<Pokemon>();
+    ArrayList<Pocion> pociones = new ArrayList<Pocion>();
 
-    public Jugador() {}
+    /**
+     * CONSTRUCTOR:
+     * Dentro de este método se mandará a llamar al llenado de datos y de los pokémon que quiere
+     * tener en su equipo, en dado caso que no quiera elegir se elegirán aleatoriamente 6.
+     * @param numJugador Indica el numero de jugador que se esta registrando para que tengan nombres distintos por defecto.
+     */
+    public Jugador(int numJugador) {
+        System.out.println("\nBienvenido al juego 'Jugador"+numJugador+"'!");
+        System.out.printf("Antes de todo, elige un mejor nombre:");
+        setNombre();
+    }
 
+    /************************************************
+     * GETTERS Y SETTERS                            *
+     ************************************************/
+    /**
+     * Permite asignar un nombre a un jugador, no requiere de parámetros porque mediante este mismo se hace el proceso
+     * de validación y asignación.
+     */
+    public void setNombre() {
+        while(this.nombre == null){
+            System.out.printf("\n ~> ");
+            this.nombre = scan.nextLine();
+            if(this.nombre.isEmpty()){
+                System.out.printf("\tOH NO! No puedes dejar este campo vacio, lo siento. Intenta otra vez.\n");
+                this.nombre = null;
+            }
+        }
+        System.out.println("\n\tActualmente tu nombre de entrenador es: "+this.getNombre()+"\n");
+        System.out.printf("Quieres cambiarlo? [Si/No] (No): \n ~> ");
+        String eleccion = scan.nextLine();
+        if ( ! eleccion.isEmpty() && (eleccion.charAt(0) == 'S' || eleccion.charAt(0) == 's')){
+            System.out.println("Entendido! Elige tu nuevo nombre: ");
+            // Se asigna un valor nulo a la instanca para que pueda volver a entrar al ciclo que permite elegir un nuevo
+            // nombre.
+            this.nombre = null;
+            setNombre();
+        }else{
+            System.out.println("De acuerdo, entonces sigamos.");
+        }
+    }
+    
+    /**
+     * Permite tener acceso al nombre del entrenador.
+     * @return Devuelve el nombre del entrenador.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Su función es mostrar en consola el equipo de pokémon que tiene el jugador.
+     */
     public void mostrarPokemon() {
         for (Pokemon pokemon : pokemones) {
             // Instrucciones
